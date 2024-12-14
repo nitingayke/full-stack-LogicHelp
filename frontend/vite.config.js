52ddef23843cdfdeb952a16574492136670a94c0
Nitin Gayke <gaykenitin975@gmail.com>
2024-12-14 21:56:51 +0530

    updated vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000, // Use Render's PORT variable or default to 3000
+    host: '0.0.0.0', // Allow external connections (necessary for Render to access the app)
+  },
 })
