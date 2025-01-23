7d3d0b8ecb579a361dee419cec85ba9a4f1fc548
Nitin Gayke <gaykenitin975@gmail.com>
2025-01-23 15:23:26 +0530

    update the cors

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ["https://logichelp.onrender.com", "http://localhost:3000"],
+        origin: ["https://logichelp.onrender.com", "http://localhost:3000", "https://logic-help.vercel.app"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
